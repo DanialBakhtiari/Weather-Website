039886697fd68e4d1e2c5095e7f883f98ed6262a
Danial Bakhtiari <danial.bakhtiari96@gmail.com>
2024-05-09 20:01:39 +0400

    Event for Input And API EndPoint Added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
-const html = document.querySelector("html")
+const html = document.querySelector("html");
+const searchbar = document.getElementById("default-search");
+const searchIcon = document.getElementById("searchIcon");
+
+searchIcon.addEventListener("click", () => {
+  let APIendPoint = `https://api.tomorrow.io/v4/weather/realtime?location=${searchbar.value}&apikey=18IdHwHitUX1eZm3w4PwFq9nG9Vy2gJ0`;
+
+  const options = { method: "GET", headers: { accept: "application/json" } };
+
+  fetch(APIendPoint, options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
+
+  searchbar.value = "";
+});
+
+searchbar.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    let APIendPoint = `https://api.tomorrow.io/v4/weather/realtime?location=${searchbar.value}&apikey=18IdHwHitUX1eZm3w4PwFq9nG9Vy2gJ0`;
+
+    const options = { method: "GET", headers: { accept: "application/json" } };
+
+    fetch(APIendPoint, options)
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
+
+    searchbar.value = "";
+  }
+});
 
 function ToggleDarkLight() {
-    
-    html.classList.toggle("dark")
-    
+  html.classList.toggle("dark");
 }
