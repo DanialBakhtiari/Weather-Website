ef58eaa3e562c23840b4378904b61ef8679cd457
Danial Bakhtiari <danial.bakhtiari96@gmail.com>
2024-05-11 14:49:01 +0400

    Some Bug Fixed

--- js/element.js
+++ js/element.js
@@ -1,4 +1,3 @@
-export {currentLoc,cityelem,cityTime,cityDate,temperature,feelsLike,sunrise,sunrset,description,humidity,windSpeed,pressureElem,UV}
 const currentLoc = document.getElementById("currentLoc");
 const cityelem = document.getElementById("cityelem");
 const cityTime = document.getElementById("cityTime");
@@ -12,3 +11,10 @@ const humidity = document.getElementById("humidity");
 const windSpeed = document.getElementById("windSpeed");
 const pressureElem = document.getElementById("pressureElem");
 const UV = document.getElementById("UV");
+const html = document.querySelector("html");
+const searchbar = document.getElementById("default-search");
+const searchIcon = document.getElementById("searchIcon");
+const dark_mode_toggle = document.getElementsByClassName("dark_mode_toggle")
+
+
+export {currentLoc,cityelem,cityTime,cityDate,temperature,feelsLike,sunrise,sunrset,description,humidity,windSpeed,pressureElem,UV,html,searchbar,searchIcon,dark_mode_toggle}
