c56cbc7506c53eda1e4a7ea1d0cd5259ee18377b
Danial Bakhtiari <danial.bakhtiari96@gmail.com>
2024-05-12 14:57:53 +0400

    Some Minor Bug Fixed

--- js/element.js
+++ js/element.js
@@ -1,13 +1,13 @@
 const currentLoc = document.getElementById("currentLoc");
-const cityelem = document.getElementById("cityelem");
+const cityElem = document.getElementById("cityElem");
 const cityTime = document.getElementById("cityTime");
 const cityDate = document.getElementById("cityDate");
 const temperature = document.getElementById("temperature");
 const feelsLike = document.getElementById("feelsLike");
-const sunrise = document.getElementById("sunrise"); 
-const sunrset = document.getElementById("sunset"); 
+const sunRise = document.getElementById("sunRise"); 
+const sunSet = document.getElementById("sunSet"); 
 const description = document.getElementById("description") ;
-const humidity = document.getElementById("humidity");
+const humidityElem = document.getElementById("humidityElem");
 const windSpeed = document.getElementById("windSpeed");
 const pressureElem = document.getElementById("pressureElem");
 const UV = document.getElementById("UV");
@@ -15,6 +15,7 @@ const html = document.querySelector("html");
 const searchbar = document.getElementById("default-search");
 const searchIcon = document.getElementById("searchIcon");
 const dark_mode_toggle = document.getElementsByClassName("dark_mode_toggle")
+const weatherCodeIcon = document.getElementById("weatherCodeIcon")
 
 
-export {currentLoc,cityelem,cityTime,cityDate,temperature,feelsLike,sunrise,sunrset,description,humidity,windSpeed,pressureElem,UV,html,searchbar,searchIcon,dark_mode_toggle}
+export {currentLoc,cityElem,cityTime,cityDate,temperature,feelsLike,sunRise,sunSet,description,humidityElem,windSpeed,pressureElem,UV,html,searchbar,searchIcon,dark_mode_toggle,weatherCodeIcon}
