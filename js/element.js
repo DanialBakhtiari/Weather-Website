0a65cabc1c3c57031aa590a7ad8e6b4237a1f7ff
Mani0185 <manisalahshoor01@gmail.com>
2024-05-12 17:04:22 +0330

    all elements selected

--- js/element.js
+++ js/element.js
@@ -4,9 +4,9 @@ const cityTime = document.getElementById("cityTime");
 const cityDate = document.getElementById("cityDate");
 const temperature = document.getElementById("temperature");
 const feelsLike = document.getElementById("feelsLike");
-const sunRise = document.getElementById("sunRise"); 
-const sunSet = document.getElementById("sunSet"); 
-const description = document.getElementById("description") ;
+const sunRise = document.getElementById("sunRise");
+const sunSet = document.getElementById("sunSet");
+const description = document.getElementById("description");
 const humidityElem = document.getElementById("humidityElem");
 const windSpeed = document.getElementById("windSpeed");
 const pressureElem = document.getElementById("pressureElem");
@@ -14,8 +14,141 @@ const UV = document.getElementById("UV");
 const html = document.querySelector("html");
 const searchbar = document.getElementById("default-search");
 const searchIcon = document.getElementById("searchIcon");
-const dark_mode_toggle = document.getElementsByClassName("dark_mode_toggle")
-const weatherCodeIcon = document.getElementById("weatherCodeIcon")
+const dark_mode_toggle = document.getElementsByClassName("dark_mode_toggle");
+const weatherCodeIcon = document.getElementById("weatherCodeIcon");
 
+const imgDaysOne = document.getElementById("imgDaysOne");
+const imgDaysTwo = document.getElementById("imgDaysTwo");
+const imgDaysThree = document.getElementById("imgDaysThree");
+const imgDaysFour = document.getElementById("imgDaysFour");
+const imgDaysFive = document.getElementById("imgDaysFive");
+const tempDaysOne = document.getElementById("tempDaysOne");
+const tempDaysTwo = document.getElementById("tempDaysTwo");
+const tempDaysThree = document.getElementById("tempDaysThree");
+const tempDaysFour = document.getElementById("tempDaysFour");
+const tempDaysFive = document.getElementById("tempDaysFive");
+const dateDaysOne = document.getElementById("dateDaysOne");
+const dateDaysTwo = document.getElementById("dateDaysTwo");
+const dateDaysThree = document.getElementById("dateDaysThree");
+const dateDaysFour = document.getElementById("dateDaysFour");
+const dateDaysFive = document.getElementById("dateDaysFive");
 
-export {currentLoc,cityElem,cityTime,cityDate,temperature,feelsLike,sunRise,sunSet,description,humidityElem,windSpeed,pressureElem,UV,html,searchbar,searchIcon,dark_mode_toggle,weatherCodeIcon}
+const hourOne = document.getElementById("hourOne");
+const hourTwo = document.getElementById("hourTwo");
+const hourThree = document.getElementById("hourThree");
+const hourFour = document.getElementById("hourFour");
+const hourFive = document.getElementById("hourFive");
+const swiperHourOne = document.getElementById("swiperHourOne");
+const swiperHourTwo = document.getElementById("swiperHourTwo");
+const swiperHourThree = document.getElementById("swiperHourThree");
+const swiperHourFour = document.getElementById("swiperHourFour");
+const swiperHourFive = document.getElementById("swiperHourFive");
+
+const imgHourOne = document.getElementById("imgHourOne");
+const imgHourTwo = document.getElementById("imgHourTwo");
+const imgHourThree = document.getElementById("imgHourThree");
+const imgHourFour = document.getElementById("imgHourFour");
+const imgHourFive = document.getElementById("imgHourFive");
+const swiperImgHourOne = document.getElementById("swiperImgHourOne");
+const swiperImgHourTwo = document.getElementById("swiperImgHourTwo");
+const swiperImgHourThree = document.getElementById("swiperImgHourThree");
+const swiperImgHourFour = document.getElementById("swiperImgHourFour");
+const swiperImgHourFive = document.getElementById("swiperImgHourFive");
+
+const tempHourOne = document.getElementById("tempHourOne");
+const tempHourTwo = document.getElementById("tempHourTwo");
+const tempHourThree = document.getElementById("tempHourThree");
+const tempHourFour = document.getElementById("tempHourFour");
+const tempHourFive = document.getElementById("tempHourFive");
+const swiperTempHourOne = document.getElementById("swiperTempHourOne");
+const swiperTempHourTwo = document.getElementById("swiperTempHourTwo");
+const swiperTempHourThree = document.getElementById("swiperTempHourThree");
+const swiperTempHourFour = document.getElementById("swiperTempHourFour");
+const swiperTempHourFive = document.getElementById("swiperTempHourFive");
+
+const windHourOne = document.getElementById("windHourOne");
+const windHourTwo = document.getElementById("windHourTwo");
+const windHourThree = document.getElementById("windHourThree");
+const windHourFour = document.getElementById("windHourFour");
+const windHourFive = document.getElementById("windHourFive");
+const swiperWindHourOne = document.getElementById("swiperWindHourOne");
+const swiperWindHourTwo = document.getElementById("swiperWindHourTwo");
+const swiperWindHourThree = document.getElementById("swiperWindHourThree");
+const swiperWindHourFour = document.getElementById("swiperWindHourFour");
+const swiperWindHourFive = document.getElementById("swiperWindHourFive");
+
+export {
+  currentLoc,
+  cityElem,
+  cityTime,
+  cityDate,
+  temperature,
+  feelsLike,
+  sunRise,
+  sunSet,
+  description,
+  humidityElem,
+  windSpeed,
+  pressureElem,
+  UV,
+  html,
+  searchbar,
+  searchIcon,
+  dark_mode_toggle,
+  weatherCodeIcon,
+  imgDaysOne,
+  imgDaysTwo,
+  imgDaysThree,
+  imgDaysFour,
+  imgDaysFive,
+  tempDaysOne,
+  tempDaysTwo,
+  tempDaysThree,
+  tempDaysFour,
+  tempDaysFive,
+  dateDaysOne,
+  dateDaysTwo,
+  dateDaysThree,
+  dateDaysFour,
+  dateDaysFive,
+  hourOne,
+  hourTwo,
+  hourThree,
+  hourFour,
+  hourFive,
+  swiperHourOne,
+  swiperHourTwo,
+  swiperHourThree,
+  swiperHourFour,
+  swiperHourFive,
+  imgHourOne,
+  imgHourTwo,
+  imgHourThree,
+  imgHourFour,
+  imgHourFive,
+  swiperImgHourOne,
+  swiperImgHourTwo,
+  swiperImgHourThree,
+  swiperImgHourFour,
+  swiperImgHourFive,
+  tempHourOne,
+  tempHourTwo,
+  tempHourThree,
+  tempHourFour,
+  tempHourFive,
+  swiperTempHourOne,
+  swiperTempHourTwo,
+  swiperTempHourThree,
+  swiperTempHourFour,
+  swiperTempHourFive,
+  windHourOne,
+  windHourTwo,
+  windHourThree,
+  windHourFour,
+  windHourFive,
+  swiperWindHourOne,
+  swiperWindHourTwo,
+  swiperWindHourThree,
+  swiperWindHourFour,
+  swiperWindHourFive,
+};
